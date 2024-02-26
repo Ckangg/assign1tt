f651b52be0fb56284ae88a3dc5eb8dc919c36fe9
Ckangg <141784836+Ckangg@users.noreply.github.com>
2024-02-25 20:45:09 -0500

    Update part_1_template_solution.py

--- part_1_template_solution.py
+++ part_1_template_solution.py
@@ -343,8 +343,8 @@ class Section1:
          3) min_samples_split, 
          4) min_samples_leaf, 
          5) max_features 
-    """
-'''
+"""
+
     def partG(
         self,
         X: NDArray[np.floating],
@@ -473,137 +473,6 @@ class Section1:
                
         """
 
-        return answer'''
- 
-        def partG(
-                self,
-                X: NDArray[np.floating],
-                y: NDArray[np.int32],
-                Xtest: NDArray[np.floating],
-                ytest: NDArray[np.int32],
-            ) -> dict[str, Any]:
-        """
-        Perform classification using the given classifier and cross validator.
-
-        Parameters:
-        - clf: The classifier instance to use for classification.
-        - cv: The cross validator instance to use for cross validation.
-        - X: The test data.
-        - y: The test labels.
-        - n_splits: The number of splits for cross validation. Default is 5.
-
-        Returns:
-        - y_pred: The predicted labels for the test data.
-
-        Note:
-        This function is not fully implemented yet.
-        """
-
-        # refit=True: fit with the best parameters when complete
-        # A test should look at best_index_, best_score_ and best_params_
-        """
-        List of parameters you are allowed to vary. Choose among them.
-         1) criterion,
-         2) max_depth,
-         3) min_samples_split, 
-         4) min_samples_leaf,
-         5) max_features 
-         5) n_estimators
-        """
-                param_grid = {
-        'criterion': ['gini', 'entropy'],
-        'max_depth': [10, 20, 30],
-        'min_samples_split': [2, 5, 10],
-        'min_samples_leaf': [1, 2, 4],
-        'max_features': ['sqrt', 'log2'],
-        #"n_estimators":[50,100,200]
-                }
-                cv = ShuffleSplit(n_splits=5, random_state=self.seed)
-                rf = RandomForestClassifier(random_state=42)
-                rf.fit(X,y)
-        # Predictions with the initial model
-                y_train_pred_orig = rf.predict(X)
-                y_test_pred_orig = rf.predict(Xtest)
-
-        # Confusion matrices
-                conf_matrix_train_orig = confusion_matrix(y, y_train_pred_orig)
-                conf_matrix_test_orig = confusion_matrix(ytest, y_test_pred_orig)
-
-        # Accuracies
-                accuracy_train_orig = nu.accuracy(conf_matrix_train_orig)
-                accuracy_test_orig = nu.accuracy(conf_matrix_test_orig)
-
-# Initialize GridSearchCV
-                grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=cv, scoring='accuracy')
-
-        # Perform grid search
-                grid_search.fit(X, y)
-                best_clf = grid_search.best_estimator_
-                accuracy = best_clf.score(Xtest,ytest)
-
-                mean_test_scores = grid_search.cv_results_['mean_test_score']
-        # Calculate the mean accuracy
-                mean_accuracy = mean_test_scores.mean()
-                best_rf_clf = best_clf
-        #best_rf_clf.fit(X,y)
-                y_train_pred_best = best_rf_clf.predict(X)
-                y_test_pred_best = best_rf_clf.predict(Xtest)
-       # Confusion matrices
-                conf_matrix_train_best = confusion_matrix(y, y_train_pred_best)
-                conf_matrix_test_best = confusion_matrix(ytest, y_test_pred_best)
-
-        # Accuracies
-                accuracy_train_best = nu.accuracy(conf_matrix_train_best)
-                accuracy_test_best = nu.accuracy(conf_matrix_test_best) 
-
-
-                answer = {
-    "clf": rf,
-    "default_parameters": rf.get_params(),
-    "best_estimator": best_clf,
-    "grid_search": grid_search,
-    "mean_accuracy_cv": mean_accuracy,
-    "confusion_matrix_train_orig": conf_matrix_train_orig,
-    "confusion_matrix_train_best": conf_matrix_train_best,
-    "confusion_matrix_test_orig": conf_matrix_test_orig,
-    "confusion_matrix_test_best": conf_matrix_test_best,
-    "accuracy_orig_full_training": accuracy_train_orig,
-    "accuracy_best_full_training": accuracy_train_best,
-    "accuracy_orig_full_testing": accuracy_test_orig,
-    "accuracy_best_full_testing": accuracy_test_best,
-        }
-
-# Now, you can print or return the answer dictionary.
-
-
-        # Enter your code, construct the `answer` dictionary, and return it.
-
-        """
-           `answer`` is a dictionary with the following keys: 
-            
-            "clf", base estimator (classifier model) class instance
-            "default_parameters",  dictionary with default parameters 
-                                   of the base estimator
-            "best_estimator",  classifier class instance with the best
-                               parameters (read documentation)
-            "grid_search",  class instance of GridSearchCV, 
-                            used for hyperparameter search
-            "mean_accuracy_cv",  mean accuracy score from cross 
-                                 validation (which is used by GridSearchCV)
-            "confusion_matrix_train_orig", confusion matrix of training 
-                                           data with initial estimator 
-                                (rows: true values, cols: predicted values)
-            "confusion_matrix_train_best", confusion matrix of training data 
-                                           with best estimator
-            "confusion_matrix_test_orig", confusion matrix of test data
-                                          with initial estimator
-            "confusion_matrix_test_best", confusion matrix of test data
-                                            with best estimator
-            "accuracy_orig_full_training", accuracy computed from `confusion_matrix_train_orig'
-            "accuracy_best_full_training"
-            "accuracy_orig_full_testing"
-            "accuracy_best_full_testing"
-               
-        """
-        # The mean accuracy of Cross validation is around 65% where as when the model trained on the enitre set, It has an 100% acccuracy, So it is higher than that of the mean accuracy of CV.
         return answer
+ 
+        
